32bde3b8fd35143b69733f5ace6a75595ec21031
thanmitsel <thanmitsel@gmail.com>
2016-12-26 13:27:45 +0200

    Delete initScale.m

--- initScale.m
+++
@@ -1,9 +0,0 @@
-function [temph, tempH, tempd, tempw, tempW, tempm, tempM]=initScale
-temph=zeros(8760,1);
-tempH=zeros(8760,24);
-tempd=zeros(365,1);
-tempw=zeros(52,1);
-tempW=zeros(52,168);
-tempm=zeros(12,1);
-tempM=zeros(12,720);
-end
