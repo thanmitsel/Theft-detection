56b203fe2a980e6db6720c1872af95720da9d7fe
thanmitsel <thanmitsel@gmail.com>
2017-03-28 21:35:19 +0300

    corrected ROC

--- ROC_curves.m
+++ ROC_curves.m
@@ -4,10 +4,12 @@ clear; close all; clc
 % 3 curves, high medium and low intensity
 % 10 points on every curve, threshold
 
-intensity=[20 50 80];
+intensity=[0.20 0.50 0.80];
 %thresh=[10 20 50 70 100 120 150 170 180 200];
 thresh=[10 20 30 40 50 60 70 80 90 100]; % ndays
-fraud_rate=0.9; % Percentage of consumers who fraud
+
+% Needs to be high or low intensity attacks are not recognised
+fraud_rate=0.9; % Percentage of consumers who fraud 
 
 DR_days=zeros(length(thresh), length(intensity));
 FPR_days=zeros(length(thresh), length(intensity));
@@ -19,13 +21,9 @@ FPR_IDs=zeros(length(thresh), length(intensity));
 
 
 % Load Data
- %cd ..\'CER Data'\Data\; %windows
  cd ../CER' Data'/Data/; %Matlab Linux
-%cd '../CER Data/Data/'; % Linux
 data=load('File1.txt');
-%cd '../../code/'; % Linux
 cd ../../Thesis/; %Matlab Linux
-%cd ..\..\code\; % windows
 
 % Sorting file
 [sData] = sortID(data);
@@ -35,7 +33,7 @@ cd ../../Thesis/; %Matlab Linux
 [hh, ID]=pickConsumers(sData);
 
 % pick some z vector 
-z=400; % Needs to be 300>
+z=100; % Needs to be 300>
 r_cons=randi(size(hh,1),z,1);
 somehh=hh(r_cons,:);
 someID=ID(r_cons,:);
@@ -44,41 +42,39 @@ someID=ID(r_cons,:);
 [h, H]=convertHours3D(somehh);
 
 for id_i=1:length(intensity)
-    for id_th=1:length(thresh)
-            %% Fraud Initialization
-            % Create Fraud data
-            F_data3D=H;
-            Y2D=zeros(size(H,1),size(H,3));
-            one_H=zeros(size(H(:,:,1)));
-            
-            [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
-            thiefs=find(fraud_idx==1);
-            for i=1:size(thiefs,1)
-                one_H=H(:,:,thiefs(i));
-                [f_data, y, F_data,Y] = type3_1Fraud(one_H, intensity(id_i));
-                F_data3D(:,:,thiefs(i))=F_data;
-                Y2D(:,thiefs(i))=Y;
-            end
-            %% Feature extraction
-            % Here we use SVM for many consumers
-
-            % Feature extraction
-            % 14 Features 
-            X=zeros(size(H,1),14,size(H,3));
-            count=0; % counts the errored data
-            for i=1:size(H,3)
-            [maxi,maxT,mini,minT,suma,av,stdev...
-                    , lfactor, mintoav,mintomax,night,skew,kurt,varia, features]=extractFeatures(F_data3D(:,:,i));
-                X(:,:,i)=features; % X NEEDS TO BE 3D, so nothing crashes later
-                if (sum(find(isnan(X(:,:,i))))~=0)
-                fprintf('There is NaN on the %dst client\n',i);
-                count=count+1;
-                % if find NaN get the values of the previous consumer
-                X(:,:,i)=X(:,:,(i-1));
-                Y2D(:,i)=Y2D(:,(i-1));
-                end
-            end
-            fprintf('\nFraud Data and features created.\n');
+    %% Fraud Initialization
+    % Create Fraud data
+    F_data3D=H;
+    Y2D=zeros(size(H,1),size(H,3));
+    one_H=zeros(size(H(:,:,1)));   
+    [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
+    thiefs=find(fraud_idx==1);
+    for i=1:size(thiefs,1)
+        one_H=H(:,:,thiefs(i));
+        [f_data, y, F_data,Y] = type3_1Fraud(one_H, intensity(id_i));
+        F_data3D(:,:,thiefs(i))=F_data;
+        Y2D(:,thiefs(i))=Y;
+    end
+    %% Feature extraction
+    % Here we use SVM for many consumers
+    % Feature extraction
+    % 14 Features 
+    X=zeros(size(H,1),14,size(H,3));       
+    count=0; % counts the errored data       
+    for i=1:size(H,3)
+         [maxi,maxT,mini,minT,suma,av,stdev...
+          , lfactor, mintoav,mintomax,night,skew,kurt,varia, features]=extractFeatures(F_data3D(:,:,i));
+         X(:,:,i)=features; % X NEEDS TO BE 3D, so nothing crashes later
+         if (sum(find(isnan(X(:,:,i))))~=0)     
+             fprintf('There is NaN on the %dst client\n',i);       
+             count=count+1;
+             % if find NaN get the values of the previous consumer       
+             X(:,:,i)=X(:,:,(i-1));       
+             Y2D(:,i)=Y2D(:,(i-1));
+         end
+    end
+    fprintf('\nFraud Data and features created.\n');
+    for id_th=1:length(thresh)        
             %% Create training and testing set
             % Choose from every consumer sample
             
@@ -91,7 +87,7 @@ for id_i=1:length(intensity)
 
             fprintf('\nSegmented Training and Testing.\n');
             % Get best fitted arguments
-            arguments=['-t ' num2str(2) ' -g ' num2str(0.1) ' -c ' num2str(10)]; 
+            arguments=['-t ' num2str(2) ' -h ' num2str(0) ' -g ' num2str(0.1) ' -c ' num2str(10)]; 
             %% SVM test and confusion matrices
             % Test SVM
             model=svmtrain(Y_train,X_train,arguments);
