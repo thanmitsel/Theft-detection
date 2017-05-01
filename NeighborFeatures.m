504153cc56c654b323f1cefa8ca87f6bb0bf5918
thanmitsel <thanmitsel@gmail.com>
2017-05-01 18:54:58 +0300

    optimize script

--- NeighborFeatures.m
+++ NeighborFeatures.m
@@ -53,17 +53,17 @@ end
 
 for i=1:size(X_1,3)
     cluster=idx(i);
-    temp_av_matrix(:,:,i)=reshape(X_1(:,1,i), [73, 5]);
-    temp_av_t(:,:,i)=temp_av_matrix(:,:,i)';
-    divided_av(:,:,i)=mean(temp_av_t(:,:,i),2);
-    if max((d_neighborhood_av(:,cluster)-divided_av(:,:,i))./d_neighborhood_av(:,cluster))>av_threshold    
-        max_dif_av(i,1)=max(d_neighborhood_av(:,cluster)-divided_av(:,:,i));
+    temp_av_matrix=reshape(X_1(:,1,i), [73, 5]);
+    temp_av_t=temp_av_matrix';
+    divided_av=mean(temp_av_t,2);
+    if max((d_neighborhood_av(:,cluster)-divided_av)./d_neighborhood_av(:,cluster))>av_threshold    
+        max_dif_av(i,1)=max(d_neighborhood_av(:,cluster)-divided_av);
     end
-    temp_std_matrix(:,:,i)=reshape(X_1(:,2,i), [73, 5]);
-    temp_std_t(:,:,i)=temp_std_matrix(:,:,i)';
-    divided_std(:,:,i)=mean(temp_std_t(:,:,i),2);
-    if max((d_neighborhood_std(:,cluster)-divided_std(:,:,i))./d_neighborhood_std(:,cluster))>std_threshold    
-        max_dif_std(i,1)=max(d_neighborhood_std(:,cluster)-divided_std(:,:,i));
+    temp_std_matrix=reshape(X_1(:,2,i), [73, 5]);
+    temp_std_t=temp_std_matrix';
+    divided_std=mean(temp_std_t,2);
+    if max((d_neighborhood_std(:,cluster)-divided_std)./d_neighborhood_std(:,cluster))>std_threshold    
+        max_dif_std(i,1)=max(d_neighborhood_std(:,cluster)-divided_std);
     end
 end
 X_3=[X_2 average_dif std_dif max_dif_av max_dif_std];
---
+++ unType1Optimize.m
@@ -0,0 +1,148 @@
+    % pick some z vector 
+    z=2000;
+    r_cons=randi(size(hh,1),z,1);
+    somehh=hh(r_cons,:);
+    someID=ID(r_cons,:);
+
+    % Convertions
+    [h, H]=convertHours3D(somehh);
+
+    %% Fraud Initialization
+    % Create Fraud data
+    F_data3D=H;
+    Y2D=zeros(size(H,1),size(H,3));
+    one_H=zeros(size(H(:,:,1)));
+
+% fraud_rate=0.05; % Percentage of consumers who fraud
+fraud_rate=0.1; % Percentage of consumers who fraud
+[normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
+thiefs=find(fraud_idx==1);
+for i=1:size(thiefs,1)    
+    intensity=1-betarnd(6,3); % beta distribution
+    dstart=floor(normrnd(size(one_H,1)/2,size(one_H,1)/6.5)); % normal distribution
+    while dstart<1 || dstart>(size(one_H,1)-1)
+        dstart=floor(normrnd(size(one_H,1)/2,size(one_H,1)/6.5)); % normal distribution
+    end
+    one_H=H(:,:,thiefs(i));
+    [f_data, y, F_data,Y] = type1_2Fraud(one_H, intensity,dstart);
+    F_data3D(:,:,thiefs(i))=F_data;
+    Y2D(:,thiefs(i))=Y;
+end
+
+% Details 
+[kWh_count, time_count, kWh_rate, time_rate] = frauDetails(H, F_data3D);
+prompt=('Apply normalization?\n 0 w/o norm, 1 with norm\n');
+apply_norm=input(prompt);
+prompt=('Apply PCA?\n 0 w/o PCA, 1 with PCA\n');
+apply_pca=input(prompt);
+%% Feature extraction
+% Here we use SVM for many consumers
+maxF1score=0;
+for i=0:0.1:1
+    av_per_dif=i;
+    for j=0:0.1:1
+        std_per_dif=j;
+        for k=0:0.1:1
+            symmetric_av=k;
+            for l=0:0.1:1
+                symmetric_std=l;
+                for w=0:0.1:1
+                    av_threshold=w;
+                    for z=0:0.1:1
+                     std_threshold=z;   
+                
+                        % Feature extraction
+                        ndays=1;
+
+                        [X_1]=ConsumerFeatures(F_data3D, av_per_dif, std_per_dif, symmetric_av, symmetric_std); %includes ONLY 3 features
+                        X_2=mean(X_1,1);
+                        X_2=permute(X_2, [3 2 1]);
+
+                        % Obtain features related to neighbors
+                        K=3;
+                        [X_3]=NeighborFeatures(X_1, X_2, K, av_threshold, std_threshold);
+                        Y1D=(sum(Y2D)>ndays)';
+                        %X=X_3(:,1:(end));
+                        X=[X_2 X_3(:,9:end)];
+                        Y=Y1D;
+
+                        fprintf('\nFraud Data and features created.\n');
+                        %% ===  PCA for Visualization ===
+                        % Use PCA to project this cloud to 2D for visualization
+                        % Subtract the mean to use PCA
+                        
+                        if apply_norm==0
+                            X_norm=X;
+                        elseif apply_norm==1
+                            [X_norm, mu, sigma] = normalizeMinus_Plus(X);
+                        end
+                        
+                        if apply_pca==0
+                            Z=X_norm;
+                        elseif apply_pca==1
+                            % PCA and project the data to 2D
+                            [U, S] = pca(X_norm);
+                            Z = projectData(X_norm, U, 2);
+
+
+                            plotClass(Z(:,:),Y(:));
+                            title('Classified examples');
+                        end
+                        fprintf('Program paused. Press enter to continue.\n');
+                        %% Create training and testing set
+                        % Choose from every consumer sample
+                        % No normarlization needed here
+
+                        P=0.3; % Percent of Test
+                        normalization=0;
+                        [X_train, Y_train, X_test, Y_test, X_full, Y_full]=pickTrainTest(Z, Y, P, normalization);
+                        Intr=sum(Y_full)/size(Y_full,1);% Probability of Intrusion based on Days
+
+                        fprintf('\nSegmented Training and Testing.\n');
+                        %% Apply anomalyDetection
+                        % Estimate mu sigma2
+                        [mu, sigma2] = estimateGaussian(X_train);
+
+                        %  Training set 
+                        p = multivariateGaussian(X_train, mu, sigma2);
+
+                        %  Cross-validation set
+                        pval = multivariateGaussian(X_test, mu, sigma2);
+
+                        %  Find the best threshold
+                        [epsilon, F1] = selectThreshold(Y_test, pval);
+                        prediction=(pval<epsilon);
+
+                        % Create confusion Matrix
+                        % Detection Rate is Recall, False Positive Rate is Inverse recall 
+                        [precision, recall, in_recall, accuracy, F1score] = confusionMatrix (Y_test, prediction);
+                        BDR=Intr*recall/(Intr*recall+(1-Intr)*in_recall) ; % Bayesian Detection Rate for days
+                        rouf_id=find(prediction==1);
+                        roufianos=someID(rouf_id); % Keeps all the ID that contain intrusion
+                        if maxF1score < F1score
+                            maxF1score=F1score;
+                            best_av_per_dif=i;
+                            best_std_per_dif=j;
+                            best_symmetric_av=k;
+                            best_symmetric_std=l;
+                            best_av_threshold=w;
+                            best_std_threshold=z;  
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+fprintf('\nBest epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('Based on best F1\nDR=%4.2f FPR=%4.2f on Cross Validation Set\n', recall, in_recall);
+fprintf('# Outliers found: %d\n', sum(p < epsilon));
+
+%% Printing Segment
+fprintf('kWh Rate %4.2fper | Time Rate %4.2fper |\n',kWh_rate,time_rate);
+fprintf('\nClassification for IDs\n');
+fprintf('| Precision %4.2f | Recall %4.2f | Accuracy %4.2f | F1score %4.2f |\n',precision,recall,accuracy,F1score);
+fprintf('| Actual Fraud %d IDs | Predicted Fraud Right %d IDs | Predicted Fraud Wrong %d IDs |\n',sum(Y_test==1),sum(prediction==1&Y_test==prediction),sum(prediction==1&Y_test~=prediction));
+fprintf(' DR  FPR  BDR  Accuracy\n%4.2f %4.2f %4.2f %4.2f \n',recall,in_recall,BDR,accuracy);
+
