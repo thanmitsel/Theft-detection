c812479f4f707af0b2db89696b32d4464623f42b
thanmitsel <thanmitsel@gmail.com>
2017-07-28 11:12:31 +0300

    LR theory

--- initFraudperMatrix.m
+++ initFraudperMatrix.m
@@ -1,6 +1,6 @@
 function [fraudDays] = initFraudperMatrix(data)
 %Works on a 2D matrix
-rate=rand(1);
+rate=rand(1); % Random rate
 nRows=size(data,1);
 nSample=floor(rate*size(data,1));
 rndIDX=randperm(nRows);
--- test2_SVM.m
+++ test2_SVM.m
@@ -16,12 +16,52 @@ someID=ID(r_cons,:);
 [d]=convertDays3D(hh);
 %% Fraud Initialization
 % Create Fraud data
-fprintf('Choose classification and press Enter.\n');
-prompt='\n0. Days\n1. Hours\n2. Half hours\n';
-x = input(prompt);
+fprintf('Choose type of attack.\n');
+prompt='\n1. Type 1\n2. Type 2\n3. Type 3\n';
+attack_type= input(prompt);
 
-fraud_rate=0.35; % Percentage of consumers who fraud
-if x==0 || x==1 
+fraud_rate=0.3; % Percentage of consumers who fraud
+if attack_type==3
+    fprintf('Choose classification and press Enter.\n');
+    prompt='\n0. Days (Fast)\n1. Hours (Default)\n2. Half hours (Slow)\n';
+    x = input(prompt);
+    if x==0 || x==1 
+        F_data3D=H;
+        f_data2D=h;
+        Y2D=zeros(size(H,1),size(H,3));
+        one_H=zeros(size(H(:,:,1)));
+
+
+        [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
+        thiefs=find(fraud_idx==1);
+        for i=1:size(thiefs,1)
+            one_H=H(:,:,thiefs(i));
+            [f_data, y, F_data,Y] = type3Fraud(one_H);
+            f_data2D(thiefs(i),:)=f_data';
+            F_data3D(:,:,thiefs(i))=F_data;
+            Y2D(:,thiefs(i))=Y;
+        end
+        % Details 
+        [kWh_count, time_count, kWh_rate, time_rate] = frauDetails(H, F_data3D);
+    elseif x==2
+        F_data3D=HH;
+        f_data2D=somehh;
+        Y2D=zeros(size(HH,1),size(HH,3));
+        one_HH=zeros(size(HH(:,:,1)));
+
+        [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
+        thiefs=find(fraud_idx==1);
+        for i=1:size(thiefs,1)
+            one_HH=HH(:,:,thiefs(i));
+            [f_data, y, F_data,Y] = type3Fraud(one_HH);
+            f_data2D(thiefs(i),:)=f_data';
+            F_data3D(:,:,thiefs(i))=F_data;
+            Y2D(:,thiefs(i))=Y;
+        end
+        % Details 
+        [kWh_count, time_count, kWh_rate, time_rate] = frauDetails(HH, F_data3D);
+    end
+elseif attack_type==1
     F_data3D=H;
     f_data2D=h;
     Y2D=zeros(size(H,1),size(H,3));
@@ -30,32 +70,20 @@ if x==0 || x==1
     
     [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
     thiefs=find(fraud_idx==1);
-    for i=1:size(thiefs,1)
+    for i=1:size(thiefs,1)    
+        intensity=1-betarnd(6,3); % beta distribution
+        dstart=floor(normrnd(size(one_H,1)/2,size(one_H,1)/6.5)); % normal distribution
+        while dstart<1 || dstart>(size(one_H,1)-1)
+            dstart=floor(normrnd(size(one_H,1)/2,size(one_H,1)/6.5)); % normal distribution
+        end
         one_H=H(:,:,thiefs(i));
-        [f_data, y, F_data,Y] = type3Fraud(one_H);
-        f_data2D(thiefs(i),:)=f_data';
+        [f_data, y, F_data,Y] = type1_2Fraud(one_H, intensity,dstart);
         F_data3D(:,:,thiefs(i))=F_data;
         Y2D(:,thiefs(i))=Y;
+        f_data2D(thiefs(i),:)=f_data;
     end
     % Details 
     [kWh_count, time_count, kWh_rate, time_rate] = frauDetails(H, F_data3D);
-elseif x==2
-    F_data3D=HH;
-    f_data2D=somehh;
-    Y2D=zeros(size(HH,1),size(HH,3));
-    one_HH=zeros(size(HH(:,:,1)));
-
-    [normal_idx, fraud_idx] = crossvalind('HoldOut', size(H,3), fraud_rate); % Keep in mind crossval floors the rate
-    thiefs=find(fraud_idx==1);
-    for i=1:size(thiefs,1)
-        one_HH=HH(:,:,thiefs(i));
-        [f_data, y, F_data,Y] = type3Fraud(one_HH);
-        f_data2D(thiefs(i),:)=f_data';
-        F_data3D(:,:,thiefs(i))=F_data;
-        Y2D(:,thiefs(i))=Y;
-    end
-    % Details 
-    [kWh_count, time_count, kWh_rate, time_rate] = frauDetails(HH, F_data3D);
 end
 
 fprintf('\nFraud Simulation done.\n');
@@ -65,16 +93,16 @@ fprintf('\nFraud Simulation done.\n');
 % No Feature extraction for cons x (time division)
 % Needs threshold
 ndays=0;
-if x==0
-    [Xn]=convertDays3D(F_data3D); % if more days than the threshold then fraud
-    ndays=0;
-elseif x==1
+if attack_type==3
+    if x==0
+        [Xn]=convertDays3D(F_data3D); % if more days than the threshold then fraud
+    else
+        Xn=f_data2D;
+    end
+else
     Xn=f_data2D;
-    ndays=0;
-elseif x==2
-    Xn=f_data2D;
-    ndays=0;    
 end
+
 Yn=(sum(Y2D)>ndays)';
     
     
@@ -129,7 +157,7 @@ fprintf('| Precision %4.2f | Recall %4.2f | Accuracy %4.2f | F1score %4.2f |\n',
 fprintf('| Actual Fraud %d IDs | Predicted Fraud Right %d IDs | Predicted Fraud Wrong %d IDs |\n',sum(Y_test==1),sum(prediction==1&Y_test==prediction),sum(prediction==1&Y_test~=prediction));
 fprintf(' DR   FPR  BDR Accuracy F1score\n%4.2f %4.2f %4.2f %4.2f   %4.2f \n',recall,in_recall,BDR,accuracy,F1score);
 
-prompt='\n-Delete train data?\n0.KEEP/n1.DELETE\n';
+prompt='\nDelete train data?\n0.KEEP\n1.DELETE\n';
 delete_data=input(prompt);
 if delete_data==1
     delete libSVM.train;
